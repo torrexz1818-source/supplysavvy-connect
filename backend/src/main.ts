664a05cd161fa89c84e25b2dea020eab17e8a5f5
Torrexz1818 <torrexz1818@gmail.com>
2026-03-22 08:10:47 -0500

    fix cors for netlify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8080'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:8080',
+      'https://supply-connect.netlify.app',
+      'https://supplysavvy-connect.netlify.app',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
