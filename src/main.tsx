6d622d22ace1a1d9cc79c6442e63fdd5f95af455
Torrexz1818 <torrexz1818@gmail.com>
2026-04-29 02:01:15 -0500

    Fix deep links on Render static site

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const redirectPath = sessionStorage.getItem("buyernodus:redirect-path");
+
+if (redirectPath) {
+  sessionStorage.removeItem("buyernodus:redirect-path");
+  window.history.replaceState(null, "", redirectPath);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
